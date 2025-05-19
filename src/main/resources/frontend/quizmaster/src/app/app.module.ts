f8492d6977db17b4bbc4ee4fc9b0d1866328088e
Tomasz <gerstel.tomasz@gmail.com>
2025-05-19 14:22:07 +0200

    added menu, separated quiz list and quiz attempt in modal

--- src/main/resources/frontend/quizmaster/src/app/app.module.ts
+++ src/main/resources/frontend/quizmaster/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.compnent';
-import { QuizComponent } from './quiz/quiz.component';
+import { QuizListComponent } from './quiz/quiz-list.component';
+import { QuizModalComponent } from './quiz/quiz-modal.component';
+import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
 import {DurationFormatPipe} from "./pipe/duration-format-pipe";
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizComponent
+    QuizListComponent,
+    QuizModalComponent,
+    AboutComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        DurationFormatPipe
+        DurationFormatPipe,
+        ModalModule.forRoot(),
     ],
   providers: [],
   bootstrap: [AppComponent]
